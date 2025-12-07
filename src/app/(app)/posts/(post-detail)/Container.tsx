d61c2a500424ebce92e76dc56d87450470ca333f
littlefish1145 <zhzabcd1024@protonmail.com>
2025-12-07 14:51:26 +0800

    Update Container.tsx

--- src/app/(app)/posts/(post-detail)/Container.tsx
+++ src/app/(app)/posts/(post-detail)/Container.tsx
@@ -1,6 +1,10 @@
+import { BackgroundGlow } from "~/components/modules/shared/BackgroundGlow"
+import type { Component } from "solid-js"
+
 export const Container: Component = ({ children }) => {
   return (
     <div className="container m-auto mt-[120px] max-w-7xl px-2 md:px-6 lg:px-4 xl:px-0">
+      <BackgroundGlow />
       {children}
     </div>
   )
